1bfbf27b99884b3c1d13f5cd5d30f4a977805628
Jerimiah Welch <jerimiah@wompmobile.com>
2023-03-21 13:20:06 -0700

    PLP CWV fetchpriority

--- src/routes/store/s/[id]/index.tsx
+++ src/routes/store/s/[id]/index.tsx
@@ -11,11 +11,12 @@ export default component$(() => {
   // const loc = useLocation();
 
   const apiProdListHrefRacoons =
-    "https://www.bedbathandbeyond.com/apis/services/composite/product-listing/v1.0/all?web3feo=1&site=BedBathUS&currencyCode=USD&country=US&rT=xtCompat&tz=420&displayAdsAt=6&web3feo=abc&q=racoons&wt=json&badge_ids=7464&url=%2Fstore%2Fs&noFacet=false&facets=%7B%7D&start=0&perPage=48&sws=&storeOnlyProducts=false&customPriceRange=false&__amp_source_origin=https%3A%2F%2Fwww.bedbathandbeyond.com";
+    "https://www.bedbathandbeyond.com/apis/services/composite/product-listing/v1.0/all?web3feo=1&site=BedBathUS&currencyCode=USD&country=US&rT=xtCompat&tz=420&displayAdsAt=6&q=racoons&wt=json&badge_ids=7464&url=%2Fstore%2Fs&noFacet=false&facets=%7B%7D&start=0&perPage=48&sws=&storeOnlyProducts=false&customPriceRange=false&__amp_source_origin=https%3A%2F%2Fwww.bedbathandbeyond.com";
   const apiProdListHrefKittens =
-    "https://www.bedbathandbeyond.com/apis/services/composite/product-listing/v1.0/all?web3feo=1&site=BedBathUS&currencyCode=USD&country=US&rT=xtCompat&tz=420&displayAdsAt=6&web3feo=abc&q=kittens&wt=json&badge_ids=7464&url=%2Fstore%2Fs&noFacet=false&facets=%7B%7D&start=0&perPage=48&sws=&storeOnlyProducts=false&customPriceRange=false&__amp_source_origin=https%3A%2F%2Fwww.bedbathandbeyond.com";
+    "https://www.bedbathandbeyond.com/apis/services/composite/product-listing/v1.0/all?web3feo=1&site=BedBathUS&currencyCode=USD&country=US&rT=xtCompat&tz=420&displayAdsAt=6&q=kittens&wt=json&badge_ids=7464&url=%2Fstore%2Fs&noFacet=false&facets=%7B%7D&start=0&perPage=48&sws=&storeOnlyProducts=false&customPriceRange=false&__amp_source_origin=https%3A%2F%2Fwww.bedbathandbeyond.com";
 
   const apiProdListTrigger = useSignal(apiProdListHrefRacoons);
+  const prodListApiMs = useSignal(0);
 
   const apiProdListResource = useResource$(async (ctx) => {
     // the resource will rerun when bar.value changes.
@@ -24,15 +25,15 @@ export default component$(() => {
       // In case the resource need to be cleaned up, this function will be called.
       // Allowing to clean resources like timers, subscriptions, fetch requests, etc.
     });
-
+    const start = Date.now();
     const prodListApiRes = await fetch(apiProdListTrigger.value, {
       headers: {
         "user-agent":
           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Womp_AMP_Generator",
       },
     });
-
     const prodListApi = await prodListApiRes.json();
+    prodListApiMs.value = Date.now() - start;
     return prodListApi;
   });
 
@@ -46,6 +47,7 @@ export default component$(() => {
           return (
             <>
               <h2>{list.fusion.q}</h2>
+              <div>API fetch time - {prodListApiMs.value / 1000} seconds</div>
               <button
                 onClick$={() =>
                   (apiProdListTrigger.value =
@@ -56,13 +58,12 @@ export default component$(() => {
               >
                 Toggle Results
               </button>
-              {list.response.docs.map((p: any) => {
+              {list.response.docs.map((p: any, i: number) => {
                 // Decode HTML encoded properties
                 // TODO - Is there a native Qwik or JSX way to do this?
                 ["DISPLAY_NAME"].forEach(
                   (prop) => (p[prop] = decode(p[prop], { level: "html5" }))
                 );
-
                 // p is a product object
                 return (
                   <a href={`/store${p.url}`} id={p.PRODUCT_ID}>
@@ -72,9 +73,11 @@ export default component$(() => {
                       class="contain absolute fill prodCardImg noLoader"
                       data-alt-img-src-id={p.altImages && p.altImages[0]}
                       data-img-src-id={p.scene7imageID}
-                      data-prod-id={p.PRODUCT_ID}
                       data-layout="responsive"
+                      data-prod-id={p.PRODUCT_ID}
+                      fetchpriority={i == 0 ? "high" : "low"}
                       height="119"
+                      loading={i == 0 ? "eager" : "lazy"}
                       src={`https://b3h2.scene7.com/is/image/BedBathandBeyond/${p.scene7imageID}?$imagePLP$&amp;wid=177&amp;hei=177`}
                       srcSet={`
 												https://b3h2.scene7.com/is/image/BedBathandBeyond/${p.scene7imageID}?$imagePLP$&amp;wid=177&amp;hei=177 177w,
